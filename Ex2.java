82f919027bbf3a6b702cd231d1cd99f0e41e819e
rivkabuskila <93676748+rivkabuskila@users.noreply.github.com>
2022-01-13 14:17:47 +0200

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,51 @@
+import api.*;
+import api.GUIG.MYFram;
+
+/**
+ * This class is the main class for Ex2 - your implementation will be tested using this class.
+ */
+public class Ex2 {
+    /**
+     * This static function will be used to test your implementation
+     *
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraph getGrapg(String json_file) {
+        DirectedWeightedGraph ans = new DWG();
+        DWGA a = new DWGA();
+        a.load(json_file);
+        ans = a.getGraph();
+        return ans;
+    }
+
+    /**
+     * This static function will be used to test your implementation
+     *
+     * @param json_file - a json file (e.g., G1.j
+     *                  json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraphAlgorithms getGrapgAlgo(String json_file) {
+        DirectedWeightedGraphAlgorithms ans = new DWGA();
+        ans.load(json_file);
+        return ans;
+    }
+
+    /**
+     * This static function will run your GUI using the json fime.
+     *
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     */
+    public static void runGUI(String json_file) {
+        DirectedWeightedGraphAlgorithms alg = getGrapgAlgo(json_file);
+        MYFram A=new MYFram(alg);
+    }
+
+    public static void main(String[] args) {
+            String x = args[0];
+             runGUI(x);
+
+        }
+
+}
